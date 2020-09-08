db0a6223bfe0f40f9c3d22cac05c53e6d4cde0fa
karthikreddy.banoor <karthikreddy.banoor@stackaero.com>
2020-09-08 13:10:23 +0530

    host updated in config file

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ def mongo_config():
 def postgres_congig():
 
     connection = psycopg2.connect(
-        user="postgres", password="password", host="172.17.0.4", port="5432", database="emp")
+        user="postgres", password="password", host="172.17.0.3", port="5432", database="emp")
 
     return connection
